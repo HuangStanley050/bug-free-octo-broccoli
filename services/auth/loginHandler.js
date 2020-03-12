6df55759a0510f96e7b9f210091c590db3bbc1a2
Stanley Huang <huang.stanley050@gmail.com>
2020-03-12 21:01:53 +1100

    able to login user with jwt result, but how to do session?

---
+++ services/auth/loginHandler.js
@@ -0,0 +1,43 @@
+import AWS from "aws-sdk";
+import util from "util";
+const { promisify } = util;
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACC_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET
+});
+
+const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: "2016-04-19",
+  region: "ap-southeast-2"
+});
+const utilPromiseAdminInitAuth = promisify(
+  COGNITO_CLIENT.adminInitiateAuth
+).bind(COGNITO_CLIENT);
+
+export const login = async (event, context) => {
+  const data = JSON.parse(event.body);
+  const { email, password } = data;
+  const userAuthParams = {
+    ClientId: process.env.AWS_CLIENT_ID,
+    UserPoolId: process.env.AWS_USER_POOL_ID,
+    AuthFlow: "ADMIN_USER_PASSWORD_AUTH",
+    AnalyticsMetadata: {
+      AnalyticsEndpointId: "STRING_VALUE"
+    },
+    AuthParameters: {
+      USERNAME: email,
+      PASSWORD: password
+    }
+  };
+  try {
+    let result = await utilPromiseAdminInitAuth(userAuthParams);
+    console.log(result);
+    return {
+      statusCode: 200,
+      body: "login route"
+    };
+  } catch (err) {
+    console.log(err);
+  }
+};
