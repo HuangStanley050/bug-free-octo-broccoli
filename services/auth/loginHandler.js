dc039e2891088d206e9a6999d27db3a4d9b66032
Stanley Huang <huang.stanley050@gmail.com>
2020-03-21 11:18:45 +1100

    able to validate token now

--- services/auth/loginHandler.js
+++ services/auth/loginHandler.js
@@ -11,17 +11,17 @@ const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider({
   apiVersion: "2016-04-19",
   region: "ap-southeast-2"
 });
-const utilPromiseAdminInitAuth = promisify(
-  COGNITO_CLIENT.adminInitiateAuth
-).bind(COGNITO_CLIENT);
+const utilPromiseAdminInitAuth = promisify(COGNITO_CLIENT.initiateAuth).bind(
+  COGNITO_CLIENT
+);
 
 export const login = async (event, context) => {
   const data = JSON.parse(event.body);
   const { email, password } = data;
   const userAuthParams = {
     ClientId: process.env.AWS_CLIENT_ID,
-    UserPoolId: process.env.AWS_USER_POOL_ID,
-    AuthFlow: "ADMIN_USER_PASSWORD_AUTH",
+    //UserPoolId: process.env.AWS_USER_POOL_ID,
+    AuthFlow: "USER_PASSWORD_AUTH",
     AnalyticsMetadata: {
       AnalyticsEndpointId: "STRING_VALUE"
     },
@@ -35,7 +35,7 @@ export const login = async (event, context) => {
     console.log(result);
     return {
       statusCode: 200,
-      body: "login route"
+      body: JSON.stringify(result.AuthenticationResult.AccessToken)
     };
   } catch (err) {
     console.log(err);
--- services/authorizer/checkAuth.js
+++ services/authorizer/checkAuth.js
@@ -1,1 +1,52 @@
-export const auth = async (event, context) => {};
+import jwt from "jsonwebtoken";
+import axios from "axios";
+import jwkToPem from "jwk-to-pem";
+
+// const generatePolicy = (principalId, effect, resource) => {
+//   const authResponse = {};
+//   authResponse.principalId = principalId;
+//   if (effect && resource) {
+//     const policyDocument = {};
+//     policyDocument.Version = "2012-10-17";
+//     policyDocument.Statement = [];
+//     const statementOne = {};
+//     statementOne.Action = "execute-api:Invoke";
+//     statementOne.Effect = effect;
+//     statementOne.Resource = resource;
+//     policyDocument.Statement[0] = statementOne;
+//     authResponse.policyDocument = policyDocument;
+//   }
+//   return authResponse;
+// };
+
+//const jwtSecret = "verySecretMuchWow";
+
+export const auth = async (event, context, callback) => {
+  const token = event.authorizationToken;
+
+  if (!token) return callback(null, "Unauthorized");
+  const tokenParts = event.authorizationToken.split(" ");
+  const tokenValue = tokenParts[1];
+
+  let result = await axios.get(
+    `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_USER_POOL_ID}/.well-known/jwks.json`
+  );
+  const pem = jwkToPem(result.data.keys[1]);
+  console.log(result.data);
+  // console.log("+++++++++++++++");
+  console.log(pem);
+  jwt.verify(tokenValue, pem, { algorithms: ["RS256"] }, function(
+    err,
+    decodedToken
+  ) {
+    if (!err) {
+      console.log("token okay");
+      console.log(decodedToken);
+    } else {
+      console.log("token not okay");
+      console.log("here is the token: ", tokenValue);
+      console.log(err);
+      callback("Token not authorized");
+    }
+  });
+};
